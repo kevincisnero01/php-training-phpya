f2482528bc20298f95fa64c3ccd712dd1e37e1d5
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2021-10-09 23:06:42 -0430

    Actualizar Plantilla

--- partials/plantilla.php
+++ partials/plantilla.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>PHP</title>
-	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 	<a class="menu menu1"  href="../">Ir a Ejercicios</a>
@@ -16,11 +16,13 @@
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum..</p>
 <?php
-	require_once"helper.php";
+    require_once"partials/helper.php";
+	require_once"partials/conexion.php";
+	
 	$con = conexion();
 ?>
 	<fieldset class="center c50">
-		<legend class="b">Registrar Estudiantes</legend>
+		<legend class="b">Titulo</legend>
 		<form action="#" method="POST">
 			<label for="name"><span class="tr">*</span> Nombre </label>
 			<input type="text" name="name" id="name" class="finput" placeholder="Ej: Jose Perez" required>
@@ -28,6 +30,26 @@
 			<p class="box-sub">Los campos con <span class="tr">*</span> son obligatorios</p>
 		</form>
 	</fieldset>
+
+	<table class="c80 center" border=1>
+	<thead>
+		<tr>
+			<th class='tcenter c20'>COL1</th>
+			<th class='pl5'>COL2</th>
+			<th class='c20'>COL3</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		
+		?>
+	</tbody>
+	<tfooter>
+		<tr>
+			<th class='tright pr10' colspan='3'>Total: <?php  ?></th>
+		</tr>
+	</tfooter>
+</table>
 
 	</div><!--container-->
 </body>
